63c54974c50fc0de19dbac558adff969e96cfeb8
Bc. Vladislav Mlejnecký <v.mlejnecky@seznam.cz>
2023-05-02 00:07:28 +0200

    OCD porn

--- include/utillib/core.h
+++ include/utillib/core.h
@@ -7,13 +7,13 @@
 #ifndef CORE_H_included
 #define CORE_H_included
 
-#include "../../src/core/src/list.h"
-#include "../../src/core/src/dynmem.h"
 #include "../../src/core/src/array.h"
-#include "../../src/core/src/error.h"
 #include "../../src/core/src/atexit.h"
-#include "../../src/core/src/check.h"
-#include "../../src/core/src/string.h"
 #include "../../src/core/src/buffer.h"
+#include "../../src/core/src/check.h"
+#include "../../src/core/src/dynmem.h"
+#include "../../src/core/src/error.h"
+#include "../../src/core/src/list.h"
+#include "../../src/core/src/string.h"
 
 #endif
--- include/utillib/utils.h
+++ include/utillib/utils.h
@@ -8,8 +8,8 @@
 #define UTILS_H_included
 
 #include "../../src/utils/src/convert.h"
-#include "../../src/utils/src/tokenizer.h"
 #include "../../src/utils/src/error_buffer.h"
 #include "../../src/utils/src/evaluate.h"
+#include "../../src/utils/src/tokenizer.h"
 
 #endif
